ec84bca652370c52ba8d064ac7d58c3ebf9171ae
florblue <florencia.otarola@gmail.com>
2018-06-17 00:18:20 -0300

    Se agrega modelo usuario y se comienza a testear usuario class

---
+++ src/models/usuario/Usuario.java
@@ -0,0 +1,36 @@
+package usuario;
+
+import apuesta.Apuesta;
+import apuesta.OpcionApuesta;
+import evento.Evento;
+
+
+public class Usuario {
+
+	private String nombre;
+
+	public Usuario(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void incrementarMontoWallet(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getMontoWallet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Apuesta hacerApuestaSegura(Evento evento, OpcionApuesta mockOpcionApuesta, float monto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void cancelarApuesta() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
