3d06eb4c565fd6ae8414f802097d129ee0f89a18
florblue <florencia.otarola@gmail.com>
2018-06-20 22:37:03 -0300

    Se completan metodos sobre apuestas seguras y se agregan handleo de  excepciones

--- src/models/usuario/Usuario.java
+++ src/models/usuario/Usuario.java
@@ -33,20 +33,32 @@ public class Usuario {
 		return wallet;
 	}
 
-	public ApuestaSegura hacerApuestaSegura(Evento evento, OpcionApuesta opcionApuesta, double monto) {
-		
-		ApuestaSegura apuesta = evento.addApuestaSegura(this, opcionApuesta, monto);
-		this.decrementarMontoWallet(monto);
-		return apuesta;
+	// usuario crea apuesta solo si el partido no ha comenzado. Accion que se delega en el evento.
+	public void hacerApuestaSegura(Evento evento, OpcionApuesta opcionApuesta, double monto) {		
+		try {
+			evento.addApuestaSegura(this, opcionApuesta, monto);
+			this.decrementarMontoWallet(monto);
+		} catch (Exception e) {
+			System.out.println("El evento ya no permite hacer apuestas.");
+			e.printStackTrace();		
+		}	
 	}
 
 	public void cancelarApuesta(ApuestaSegura apuestaACancelar) {
-		apuestaACancelar.getEvento().cancelarApuestaSegura(apuestaACancelar);
-	
+		try {
+			apuestaACancelar.getEvento().cancelarApuestaSegura(apuestaACancelar);
+		} catch (Exception e) {
+			System.out.println("Una apuesta no puede ser cancelada si el partido ha finalizado.");
+			e.printStackTrace();
+		}	
 	}
 
 	public void reactivarApuesta(ApuestaSegura apuestaAReactivar) {
-		apuestaAReactivar.getEvento().reactivarApuestaSegura(apuestaAReactivar);
-	
+		try {
+			apuestaAReactivar.getEvento().reactivarApuestaSegura(apuestaAReactivar);
+		} catch (Exception e) {
+			System.out.println("Una apuesta solo puede ser reactivada si el partido aún no ha comenzaado.");
+			e.printStackTrace();
+		}	
 	}
 }
