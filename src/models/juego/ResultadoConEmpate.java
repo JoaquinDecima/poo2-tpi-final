14f63dc06e2dd3ee20100863c3966d9fbaaf7af3
florblue <florencia.otarola@gmail.com>
2018-06-22 02:08:55 -0300

    Se agrega sublcase ResultadoConEmpate para representar los resultados de partidos cuyos deportes contemplan la posibilidad de empate

---
+++ src/models/juego/ResultadoConEmpate.java
@@ -0,0 +1,12 @@
+package juego;
+
+import competidor.Competidor;
+
+public class ResultadoConEmpate extends Resultado {
+
+	public ResultadoConEmpate(Competidor competidorGana, Competidor competidorPierde, boolean esEmpate) {
+
+		
+	}
+
+}
