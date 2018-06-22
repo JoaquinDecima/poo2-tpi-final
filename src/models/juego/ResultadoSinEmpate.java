0f6a32eff8edf22931abf5c891af775bf7ee469b
florblue <florencia.otarola@gmail.com>
2018-06-22 02:09:56 -0300

    Se agrega subclase ResultadoSinEmpate para representar aquellos resultados de partidos cuyos deportes no permiten empate

---
+++ src/models/juego/ResultadoSinEmpate.java
@@ -0,0 +1,12 @@
+package juego;
+
+import competidor.Competidor;
+
+public class ResultadoSinEmpate extends Resultado {
+
+	public ResultadoSinEmpate(Competidor competidorGana, Competidor competidorPierde) {
+
+		// TODO Auto-generated constructor stub
+	}
+
+}
